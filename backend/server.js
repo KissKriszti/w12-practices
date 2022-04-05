93b9677df816e6f8f949d9e38dae472fd5896f6a
Kriszti <kecskeskriszti22@gmail.com>
2022-04-05 17:53:05 +0200

    updated

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,17 @@ const app = express();
 const port = 9000;
 const userFile = path.join(`${__dirname}/../frontend/users.json`)
 
+const fFolder = path.join(`${__dirname}/../frontend`);
+
+app.use(express.json());
+app.use('/public', express.static(`${fFolder}/public`));
+
 app.get('/', (req,res,next) => {
+    console.log("Request received");
+    res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+})
+
+app.get('/admin/order-view', (req,res,next) => {
     console.log("Request received");
     res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
 })
@@ -73,7 +83,26 @@ app.get('/api/v1/users/:status', (req,res,next) => {
     }
 })
 
-app.use('/public', express.static(`${__dirname}/../frontend/public`));
+app.post('/users/new', (req,res) => {
+    fs.readFile(`${fFolder}/users.json`, (error,data) => {
+        if (error) {
+            console.log(error);
+            res.send("Error reading users file");
+        } else {
+            const users = JSON.parse(data);
+            console.log(req.body)
+            users.push(req.body);
+
+            fs.writeFile(`${fFolder}/users.json`, JSON.stringify(users), error => {
+                if (error) {
+                    console.log(error);
+                    res.send("Error writing users file");
+                }
+            })
+            res.send(req.body)
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`http://127.0.0.1:${port}`)
--- frontend/public/script.js
+++ frontend/public/script.js
@@ -12,11 +12,54 @@ const userComponent = ({name,surname}) => {
     `;
 };
 
+const endUserComponent = () => {
+    return `
+    <div>
+        <input type="text" placeholder="First Name" name="name">
+        <input type="text" placeholder="Last Name" name="surname">
+        <button id="send">Send</button>
+    </div>
+    `
+}
+
 window.addEventListener('load', async () => {
     const result = await parseJSON('/api/v1/users');
+
+    if(window.location.pathname === '/admin/order-view') {
+        console.log("We are at the admin page")
+    }
+    else {
+        console.log("We are at the customer page")
+    }
+
     const rootElement = document.getElementById("root");
     rootElement.insertAdjacentHTML(
         'beforeend', 
         result.map(user => userComponent(user)).join("")
     )
+    rootElement.insertAdjacentHTML('afterend', endUserComponent())
+
+    const sendBtn = document.getElementById("send");
+    sendBtn.addEventListener('click', () => {
+
+        const userData = {
+            name: document.querySelector(`input[name='name']`).value,
+            surname: document.querySelector(`input[name='surname']`).value
+        };
+
+        const fetchSettings = {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userData)
+        };
+
+        fetch('/users/new', fetchSettings)
+        .then(async data => {
+            const user = await data.json();
+
+            rootElement.insertAdjacentHTML('beforeend', userComponent(user));
+        });
+    });
 });
